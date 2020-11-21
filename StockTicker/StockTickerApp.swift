2fb006c821eb3372583878a23a620d17f01a6fcd
Aijun Li <liaijun.cn@gmail.com>
2020-11-21 23:41:58 +0800

    Initial Commit

---
+++ StockTicker/StockTickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTickerApp.swift
+//  StockTicker
+//
+//  Created by 李爱军 on 2020/11/21.
+//
+
+import SwiftUI
+
+@main
+struct StockTickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
