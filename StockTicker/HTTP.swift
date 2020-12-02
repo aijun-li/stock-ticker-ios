6a38544d1119977d5f21d24b115444546d101b39
Aijun Li <liaijun.cn@gmail.com>
2020-12-02 15:34:58 +0800

    Add optional value check in HTTP requests

--- StockTicker/HTTP.swift
+++ StockTicker/HTTP.swift
@@ -12,25 +12,44 @@ import SwiftyJSON
 struct HTTP {
     static func getLatestPrice(tickers: String, callback: @escaping (JSON) -> Void) {
         AF.request("https://mystockticker-pro-225417.wm.r.appspot.com/api/details/latest?tickers=\(tickers)").validate().responseJSON { response in
-            callback(JSON(response.data!))
+            if let data = response.data {
+                callback(JSON(data))
+            } else {
+                callback(JSON(parseJSON: "[]"))
+                print("HTTP error: Fetching latest price failed!\n")
+            }
         }
     }
     
     static func getSuggestions(keyword: String, callback: @escaping (JSON) -> Void) {
         AF.request("https://mystockticker-pro-225417.wm.r.appspot.com/api/suggestions/\(keyword)").validate().responseJSON { response in
-            callback(JSON(response.data!))
+            if let data = response.data {
+                callback(JSON(data))
+            } else {
+                callback(JSON(parseJSON: "[]"))
+            }
         }
     }
     
     static func getMeta(ticker: String, callback: @escaping (JSON) -> Void) {
         AF.request("https://mystockticker-pro-225417.wm.r.appspot.com/api/details/meta/\(ticker)").validate().responseJSON { response in
-            callback(JSON(response.data!))
+            if let data = response.data {
+                callback(JSON(data))
+            } else {
+                callback(JSON(parseJSON: "{}"))
+                print("HTTP error: Fetching meta info failed!\n")
+            }
         }
     }
     
     static func getNews(ticker: String, callback: @escaping (JSON) -> Void) {
         AF.request("https://mystockticker-pro-225417.wm.r.appspot.com/api/news/\(ticker)").validate().responseJSON { response in
-            callback(JSON(response.data!))
+            if let data = response.data {
+                callback(JSON(data))
+            } else {
+                callback(JSON(parseJSON: "[]"))
+                print("HTTP error: Fetching news failed!\n")
+            }
         }
     }
 }
